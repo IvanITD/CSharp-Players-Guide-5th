a4aef442c80a45e446d36a87bf797065a1e4d8d7
Ivan Ivanov <ivan78608@gmail.com>
2022-12-01 01:41:05 +0200

    Add project files.

---
+++ Part 1 - The Basics/Level 3 - Hello World - Your First Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Level_3___Hello_World___Your_First_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
