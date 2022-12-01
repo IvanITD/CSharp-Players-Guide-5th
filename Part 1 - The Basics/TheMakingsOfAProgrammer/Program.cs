c013b01b75006a89c56fa42e946376bc31cd6b51
Ivan Ivanov <ivan78608@gmail.com>
2022-12-02 01:10:57 +0200

    Challenge - The Makings of a Programmer - 50XP

---
+++ Part 1 - The Basics/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,8 @@
+﻿// Make a program with 5 Console.WriteLine statements in it.
+// Answer this question: How many statements do you think a program can contain?
+
+Console.WriteLine("A program ");
+Console.WriteLine("can contain ");
+Console.WriteLine("as many statements ");
+Console.WriteLine("as the ");
+Console.WriteLine("user wants!");
