0f970546c21bc338318aa8830ad0eb1d130c51ba
Ivan Ivanov <ivan78608@gmail.com>
2022-12-06 01:18:50 +0200

    Challenge - The Thing Namer 3000 - 100XP Without the bug fixed I need to find the bug and fix it!!!

---
+++ Part 1 - The Basics/TheThingNamer3000/Program.cs
@@ -0,0 +1,20 @@
+﻿// Prints text to the console
+Console.WriteLine("What kind of thing are we talking about?");
+
+// Reads User's text input from the console
+string a = Console.ReadLine();
+
+// Prints text to the console
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+// Reads User's text from the console
+string b = Console.ReadLine();
+
+// Stores the text, "of Doom", into a varible of type 'string'
+string c = "of Doom";
+
+// Stores the text, "3000", into a variable of type 'string'
+string d = "3000";
+
+// Prints text to the console, while using interpolation
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
