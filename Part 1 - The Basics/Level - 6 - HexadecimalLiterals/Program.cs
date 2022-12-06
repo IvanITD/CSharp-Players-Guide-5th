8bb8a94e9ef98ca84eae13ced23576cbc4339e1c
Ivan Ivanov <ivan78608@gmail.com>
2022-12-06 11:28:15 +0200

    The exercise hasn't been finished

---
+++ Part 1 - The Basics/Level - 6 - HexadecimalLiterals/Program.cs
@@ -0,0 +1,33 @@
+﻿// Side Quest - Hexadecimal Literals
+// Write the numbers from 1 to 10 in programmers way
+//      a) base 2 (binary digits)
+    int zero = 0b0000;
+    int one = 0b0001;
+    int two = 0b0010;
+    int three = 0b0011;
+    int four = 0b0100;
+    int five = 0b0101;
+    int six = 0b0110;
+    int seven = 0b0111;
+    int eight = 0b1000;
+    int nine = 0b1001;
+    int ten = 0b1010;
+
+//      b) base 16 (hexadecimal digits)
+    int zero0 = 0xFF00FF;
+   /* int one1 = 0x;
+    int two2 = 0x;
+    int three3 = 0x;
+    int four4 = 0x;
+    int five5 = 0x;
+    int six6 = 0x;
+    int seven7 = 0x;
+    int eight8 = 0x;
+    int nine9 = 0x;
+    int ten10 = 0x;
+   */
+
+
+Console.WriteLine($"{zero} {zero0}");
+
+// I need to write the Hexadecimal numbers in programmers way
