103ee340281491fa3eaa09e63b9ca5ff05e88494
Ivan Ivanov <ivan78608@gmail.com>
2022-12-10 02:39:24 +0200

    Exercise Solved Challenge - The Varibale Shop - 100XP

---
+++ Part 1 - The Basics/TheVariableShop/Program.cs
@@ -0,0 +1,41 @@
+﻿    int myInteger = 5;
+Console.WriteLine($"My Integer: {myInteger}");
+
+    short myShort = 4050;
+Console.WriteLine($"My Short: {myShort}");
+
+    long myLong = 2385390537593;
+Console.WriteLine($"My Long: {myLong}");
+
+    byte myByte = 150;
+Console.WriteLine($"My Byte: {myByte}");
+
+    sbyte mySByte = 127;
+Console.WriteLine($"My Sbyte: {mySByte}");
+
+    uint myUInt = 467;
+Console.WriteLine($"My Uint: {myUInt}");
+
+    ushort myUShort = 65436;
+Console.WriteLine($"My Ushort: {myUShort}");
+
+    ulong myULong = 123456789123UL;
+Console.WriteLine($"My Ulong: {myULong}");
+
+    char myChar = 'I';
+Console.WriteLine($"My Char: {myChar}");
+
+    string myString = "Ivan";
+Console.WriteLine($"My String: {myString}");
+
+    float myFloat = 150.99f;
+Console.WriteLine($"My Float: {myFloat}");
+
+    double myDouble = 1.50;
+Console.WriteLine($"My Double: {myDouble}");
+
+    decimal myDecimal = 220.43m;
+Console.WriteLine($"My Decimal: {myDecimal}");
+
+    bool myBool = true;
+Console.WriteLine($"My Bool: {myBool}");
