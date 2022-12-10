2e8cb698a4744eb0fbd87afe60a6358899fdbc17
Ivan Ivanov <ivan78608@gmail.com>
2022-12-10 03:05:26 +0200

    Challenge - The Varible Shop Returns - 50XP Exercise Solved Modified Variable Shop

--- Part 1 - The Basics/TheVariableShop/Program.cs
+++ Part 1 - The Basics/TheVariableShop/Program.cs
@@ -1,41 +1,55 @@
 ﻿    int myInteger = 5;
+myInteger = 6;
 Console.WriteLine($"My Integer: {myInteger}");
 
     short myShort = 4050;
+myShort = 4051;
 Console.WriteLine($"My Short: {myShort}");
 
     long myLong = 2385390537593;
+myLong = 2385390537592;
 Console.WriteLine($"My Long: {myLong}");
 
     byte myByte = 150;
+myByte = 149;
 Console.WriteLine($"My Byte: {myByte}");
 
     sbyte mySByte = 127;
+mySByte = 126;
 Console.WriteLine($"My Sbyte: {mySByte}");
 
     uint myUInt = 467;
+myUInt = 466;
 Console.WriteLine($"My Uint: {myUInt}");
 
     ushort myUShort = 65436;
+myUShort = 65435;
 Console.WriteLine($"My Ushort: {myUShort}");
 
     ulong myULong = 123456789123UL;
+myULong = 123456789122L;
 Console.WriteLine($"My Ulong: {myULong}");
 
     char myChar = 'I';
+myChar = 'v';
 Console.WriteLine($"My Char: {myChar}");
 
     string myString = "Ivan";
+myString = "Ivanov";
 Console.WriteLine($"My String: {myString}");
 
     float myFloat = 150.99f;
+myFloat = 150.98f;
 Console.WriteLine($"My Float: {myFloat}");
 
     double myDouble = 1.50;
+myDouble = 1.49;
 Console.WriteLine($"My Double: {myDouble}");
 
     decimal myDecimal = 220.43m;
+myDecimal = 220.42m;
 Console.WriteLine($"My Decimal: {myDecimal}");
 
     bool myBool = true;
+myBool = false;
 Console.WriteLine($"My Bool: {myBool}");
