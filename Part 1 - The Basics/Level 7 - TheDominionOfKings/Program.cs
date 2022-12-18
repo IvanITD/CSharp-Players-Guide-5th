abc09cf26e53e66b27c07c9039f4c8130841ccdc
Ivan Ivanov <ivan78608@gmail.com>
2022-12-18 23:33:48 +0200

    Solved Exercise Challenge - The Dominion of Kings - 100XP

---
+++ Part 1 - The Basics/Level 7 - TheDominionOfKings/Program.cs
@@ -0,0 +1,11 @@
+﻿int provinces = int.Parse(Console.ReadLine());
+int duchies = int.Parse(Console.ReadLine());
+int estates = int.Parse(Console.ReadLine());
+
+provinces+= 6;
+duchies+= 3;
+estates++;
+
+Console.WriteLine($"Provinces: {provinces}");
+Console.WriteLine($"Duchies: {duchies}");
+Console.WriteLine($"Estates: {estates}");
