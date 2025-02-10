b8a7ce1936ecb0fa0b280a2cb753e0ba460f445a
Ivan Ivanov <ivan78608@gmail.com>
2025-02-10 03:25:01 +0200

    Solved the challenge named "The Making of a Programmer"

---
+++ Part 1 - The Basics/Level 3 - Hello World Your First Program/3. Challenge/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine("I'm a programmer now!");
+Console.WriteLine("Time to write some code!");
+Console.WriteLine("Console.WriteLine(\"Hello World!\");");
+Console.WriteLine("Console.WriteLine(\"I'm a programmer now!\");");
