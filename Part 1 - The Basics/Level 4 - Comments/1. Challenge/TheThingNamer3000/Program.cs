94712bb3baeabfdddb088158b94154952a34008b
Ivan Ivanov <ivan78608@gmail.com>
2025-02-11 03:26:21 +0200

    Solved the Challenge called "The Thing Namer 3000"

---
+++ Part 1 - The Basics/Level 4 - Comments/1. Challenge/TheThingNamer3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind if thing are we talking about?"); //Ask the user what kind of thing they are talking about
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?"); /* Ask the user how they would describe the thing.*/
+string b = Console.ReadLine();
+string c = "Doom"; //This is the description of the thing
+string d = "3000"; /* This is the version of the thing */
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
