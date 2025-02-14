956ecb87cfc5759fc5e42052a2e9887e3d43cfb1
Ivan Ivanov <ivan78608@gmail.com>
2025-02-14 11:34:56 +0200

    Created a C# Console Program, to write the answer for the Side Quest named "Binary and Hexadecimal Literals, and re-created it the same way it was written in the Readme file

---
+++ Part 1 - The Basics/Level 6 - The C# Type System/BinaryAndHexadecimalLiterals/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("+---------+------------+--------------+");
+Console.WriteLine("| Decimal |  Binary    | Hexadecimal  |");
+Console.WriteLine("+---------+------------+--------------+");
+
+Console.WriteLine($"| {0,7} | {Convert.ToString(0, 2).PadLeft(4, '0'),10} | 0x{0:X,-11}       |");
+Console.WriteLine($"| {1,7} | {Convert.ToString(1, 2).PadLeft(4, '0'),10} | 0x{1:X,-11}       |");
+Console.WriteLine($"| {2,7} | {Convert.ToString(2, 2).PadLeft(4, '0'),10} | 0x{2:X,-11}       |");
+Console.WriteLine($"| {3,7} | {Convert.ToString(3, 2).PadLeft(4, '0'),10} | 0x{3:X,-11}       |");
+Console.WriteLine($"| {4,7} | {Convert.ToString(4, 2).PadLeft(4, '0'),10} | 0x{4:X,-11}       |");
+Console.WriteLine($"| {5,7} | {Convert.ToString(5, 2).PadLeft(4, '0'),10} | 0x{5:X,-11}       |");
+Console.WriteLine($"| {6,7} | {Convert.ToString(6, 2).PadLeft(4, '0'),10} | 0x{6:X,-11}       |");
+Console.WriteLine($"| {7,7} | {Convert.ToString(7, 2).PadLeft(4, '0'),10} | 0x{7:X,-11}       |");
+Console.WriteLine($"| {8,7} | {Convert.ToString(8, 2).PadLeft(4, '0'),10} | 0x{8:X,-11}       |");
+Console.WriteLine($"| {9,7} | {Convert.ToString(9, 2).PadLeft(4, '0'),10} | 0x{9:X,-11}       |");
+Console.WriteLine($"| {10,7} | {Convert.ToString(10, 2).PadLeft(4, '0'),10} | 0x{10:X,-11}       |");
+
+Console.WriteLine("+---------+------------+--------------+");
